acd11559d620d3f9f2218d2158d8736d59453495
Juan Lladó <jllado@gmail.com>
2017-02-23 17:08:19 +0100

    remove.sh: now control some errors

--- myjenkins/remove.sh
+++ myjenkins/remove.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-docker stop $(docker ps -aq --filter ancestor=myjenkins)
-docker rm -f $(docker ps -aq --filter ancestor=myjenkins)
-docker rmi -f $(docker images -aq --filter reference=myjenkins)
+if [ $(docker ps -aq --filter ancestor=myjenkins) ]; then
+    docker stop $(docker ps -aq --filter ancestor=myjenkins)
+    docker rm -f $(docker ps -aq --filter ancestor=myjenkins)
+fi
+if [ $(docker images -aq --filter reference=myjenkins) ]; then
+    docker rmi -f $(docker images -aq --filter reference=myjenkins)
+else
+    echo Already removed!
+fi
 
 
